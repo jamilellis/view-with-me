82eb20ba06b1aa56406b0fa57270d1273bf9606d
Ellis, Jamil (HBO) <Jamil.Ellis@hbo.com>
2014-03-19 13:30:52 -0400

    cleaned up code for socket.io and dash

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -23,8 +23,42 @@ angular.module('viewWithMeApp')
      };
 
      $scope.setupVideo = function(){
+         var vars = $scope.getUrlVars(),
+             url = "http://dash.edgesuite.net/envivio/dashpr/clear/Manifest.mpd",
+             video,
+             context,
+             player;
 
+         if (vars && vars.hasOwnProperty("url")) {
+             url = vars.url;
+         }
+
+         video = document.querySelector(".dash-video-player video");
+         context = new Dash.di.DashContext();
+         player = new MediaPlayer(context);
+
+         player.startup();
+
+         player.attachView(video);
+         player.setAutoPlay(false);
+
+         player.attachSource(url);
      };
+
+
+
+        $scope.getUrlVars = function(){
+            var vars = {};
+            var parts = window.location.href.replace(/[?&]+([^=&]+)=([^&]*)/gi, function(m,key,value) {
+                vars[key] = value;
+            });
+            return vars;
+        }
+
+
+
+
+
 
      $scope.init();
 
